9cb08acde7de0b16b31df6930945d4a23e2a2c71
john dowling <john.dowling@diskarchive.com>
2025-04-12 19:56:09 +0100

    adding disk by groups adding groups and group information

--- alto.py
+++ alto.py
@@ -1,9 +1,9 @@
 import ipaddress
-
-import error
+import uuid
 import tcp_connection
 import disk
 import typing
+import group
 
 
 class AltoIII(tcp_connection.TcpConnection):
@@ -48,3 +48,27 @@ class AltoIII(tcp_connection.TcpConnection):
         :return:
         """
         pass
+
+    def get_disk_by_group(self, group_name: str) -> typing.List[disk.Disk]:
+        disks = self._error_check_list(self._send_tcp(f"get|disks|{group_name}"))
+        return [
+            disk.Disk(disk_uuid=uuid.UUID(d.split(":")[0]), ip_address=self.ip_address, port=self.port) for d in disks
+        ]
+
+    def get_groups(self) -> typing.List[str]:
+        """
+        Get a list of groups on the alto device
+        :return:
+        """
+        return self._error_check_list(self._send_tcp("get|groups"))
+
+    def get_group_info(self, group_name: str) -> group.Group:
+        """
+        Get Information on a specific group from the alto device
+        :return:
+        """
+        # ToDo: The group information needs to be determined and added to the Group Dataclass
+        group_info = self._error_check(self._send_tcp(f"get|groupinfo|{group_name}"))
+        return group.Group(
+            name=group_name
+        )
--- disk.py
+++ disk.py
@@ -1,6 +1,7 @@
 import uuid
 import tcp_connection
 import ipaddress
+import typing
 
 
 class Disk(tcp_connection.TcpConnection):
@@ -14,7 +15,7 @@ class Disk(tcp_connection.TcpConnection):
             Bring the Current Disk online
         :return:
         """
-        self._error_check(self._send_tcp(f"opendisk|{str(self.uuid)}"))
+        self._error_check(self._send_tcp(f"get|disk|str({self.uuid})|1"))
 
     def offline(self):
         """
@@ -22,3 +23,11 @@ class Disk(tcp_connection.TcpConnection):
             :return:
         """
         self._error_check(self._send_tcp(f"closedisk|{str(self.uuid)}"))
+
+    def get_files_in_location(self) -> typing.List[str]:
+        """
+            Get all the from the specified location
+        :param location:
+        :return:
+        """
+        return self._error_check_list(self._send_tcp(f"get|files|{self.uuid}|1"))

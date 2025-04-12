6e0c4ad3267381002defdaa5d087c8715ff3f7f7
john dowling <john.dowling@diskarchive.com>
2025-04-12 19:18:39 +0100

    Adding disk open and close commands

--- disk.py
+++ disk.py
@@ -5,21 +5,20 @@ import ipaddress
 
 class Disk(tcp_connection.TcpConnection):
 
-    def __init__(self, ip_address: ipaddress.IPv4Address, port: int) -> None:
+    def __init__(self, disk_uuid: uuid.UUID, ip_address: ipaddress.IPv4Address, port: int) -> None:
         super().__init__(ip_address, port)
-
-        self.id: uuid.UUID
+        self.uuid: uuid.UUID = disk_uuid
 
     def online(self):
         """
             Bring the Current Disk online
         :return:
         """
-        pass
+        self._error_check(self._send_tcp(f"opendisk|{str(self.uuid)}"))
 
     def offline(self):
         """
             Take the Disk Offline
             :return:
         """
-        pass
+        self._error_check(self._send_tcp(f"closedisk|{str(self.uuid)}"))
